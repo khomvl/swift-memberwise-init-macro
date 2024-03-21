55c6dd762b2024ce951040acea184455c96eba16
Vladislav Khomiakov <khomiakov@nutson.us>
2024-03-21 14:06:49 +0300

    - Bump swift-syntax to 510.0.1

--- Package.swift
+++ Package.swift
@@ -28,7 +28,7 @@ let package = Package(
     //    ),
     // TODO: w/f https://github.com/pointfreeco/swift-macro-testing/pull/8
     .package(url: "https://github.com/pointfreeco/swift-macro-testing", from: "0.2.1"),
-    .package(url: "https://github.com/apple/swift-syntax", from: "509.0.0"),
+    .package(url: "https://github.com/apple/swift-syntax", from: "510.0.1"),
   ],
   targets: [
     .macro(
